4e1ebc306b11af032a7c8bdd8ae26309d73f6ca0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-02-09 18:04:35 +0900

    소켓통신 기본코드

--- app/src/main/java/com/example/socket/MainActivity.kt
+++ app/src/main/java/com/example/socket/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.socket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.socket.databinding.ActivityMainBinding
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 
 class MainActivity : AppCompatActivity() {
+    private var mbinding: ActivityMainBinding ?= null
+    private val binding get() = mbinding!!
+
+    lateinit var socket: Socket
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        socket = SocketApplication.get()
+        socket.connect()
     }
 }
