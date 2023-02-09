ec7777ee0f0ab19f191fd922ed6d69afe552904b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-02-09 23:00:42 +0900

    파이어베이스로 메시지앱 만들기(소켓으로는 나중에...)

--- app/src/main/java/com/example/socket/MainActivity.kt
+++ app/src/main/java/com/example/socket/MainActivity.kt
@@ -2,22 +2,34 @@ package com.example.socket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.socket.databinding.ActivityMainBinding
-import io.socket.client.Socket
-import io.socket.emitter.Emitter
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private var mbinding: ActivityMainBinding ?= null
     private val binding get() = mbinding!!
 
-    lateinit var socket: Socket
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("message")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
 
-        socket = SocketApplication.get()
-        socket.connect()
+            override fun onCancelled(error: DatabaseError) {
+                //애러 떳을때
+                Log.d("상태", error.message)
+            }
+        })
     }
 }
