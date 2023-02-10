6ec4577dbd773d53014e446ec25cde668d6be0ac
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-02-10 18:11:08 +0900

    메시지 받을 다른 앱 만들기()

--- message/src/main/java/com/example/message/MainActivity.kt
+++ message/src/main/java/com/example/message/MainActivity.kt
@@ -2,14 +2,48 @@ package com.example.message
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.message.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private var mbinding: ActivityMainBinding ?= null
     private val binding get() = mbinding!!
+
+    private var data = mutableListOf<Chatting>()
+    private val adapter = MessageAdapter(data)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference("message/text")
+
+        binding.recyclerview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+        binding.recyclerview.adapter = adapter
+
+        binding.button.setOnClickListener {
+            data.add(Chatting(binding.message.text.toString()))
+            ref.setValue(data)
+            binding.message.text = null
+            adapter.notifyDataSetChanged()
+        }
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.d("상태","onDataChange")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                //애러 떳을때
+                Log.d("상태", error.message)
+                Toast.makeText(this@MainActivity,"${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
